f88997af3346b41fd70683dd0534d35e61be6a0c
Terry Gunderson <tgunderson25@gmail.com>
2015-04-28 12:12:44 -0500

    Finished functionality, need to style

--- script.js
+++ script.js
@@ -1,15 +1,36 @@
-var apikey = ''; // Put your API key here
+var apikey = '728618f4cc37c292e2a09339bb33711dd540ddbd'; // Put your API key here
 
-
-// Use this function to do stuff with your results. 
+// Use this function to do stuff with your results.
 // It is called after 'search' is executed.
 function searchCallback(results) {
     console.log(results);
+	for(var i = 0; i < 10; i++) {
+		var description = "";
+		if (results[i].deck){
+			description = results[i].deck;
+		}
+		if (results[i].image){
+			var image_url = results[i].image.icon_url;
+		}
+		$('.row').last().append("<div class='col-md-4 info'><img class='hidden-sm hidden-xs' src='"+ image_url + "'><p class='lead well'>" + results[i].name + "</p><div class='result'>" + description + "</div><button class='btn btn-sm btn-success removeTitle'>Remove Title</button></div>");
+		if (i % 3 == 0){
+			$('.container').append("<div class='row'></div>");
+		}
+		$('.row').hide().delay(i * 500).fadeIn('slow');
+	}
+	$('.info').on('click', '.removeTitle', function(){
+		console.log("Remove button");
+		console.log($(this).parent().children('p'));
+		$(this).parent().children('p').fadeOut('slow', function(){
+			$(this).remove();
+		});
+	});
 }
 
 $(document).ready(function() {
 
 	// Start the search here!
+	search('batman');
 	
 });
 
